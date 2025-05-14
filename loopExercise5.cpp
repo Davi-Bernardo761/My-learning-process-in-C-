7a8ce17e0b95e10edadffd2c02d2b6abadbdc8a2
Davi-Bernardo761 <davibernardotrabalho@gmail.com>
2025-05-14 19:52:21 -0300

    Update loopExercise5.cpp

--- loopExercise5.cpp
+++ loopExercise5.cpp
@@ -1,9 +1,9 @@
-//This program takes 3 numbers from the user and calculates it's avarage value.
+//This program takes 3 numbers from the user, calculates it's average value, and prints on the screen.
 #include <iostream>
 #include <string>
 using namespace std;
 int main(){
-    int number = 0 ,storage = 0,avarage = 0;
+    double number = 0 ,storage = 0,avarage = 0;
     for (int counter = 0; counter < 3; counter++) {
         cout<<"Insert a value."<<endl;
         cin>>number;

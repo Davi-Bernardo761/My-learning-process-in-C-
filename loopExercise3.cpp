f2c55daa97a09db6db399606bd2c84db17fe14fc
Davi-Bernardo761 <davibernardotrabalho@gmail.com>
2025-05-14 19:33:46 -0300

    Update loopExercise3.cpp

--- loopExercise3.cpp
+++ loopExercise3.cpp
@@ -1,9 +1,10 @@
+//This program adds up 4 integers given by the user and prints the result on screen.
 #include <iostream>
 #include <string>
 using namespace std;
 int main(){
-    int value, storage;
-    for (int counter = 0; counter < 5; counter++) {
+    int value = 0, storage = 0;
+    for (int counter = 1; counter < 5; counter++) {
         cout<<"Insert a value."<<endl;
         cin>>value;
         storage +=value;

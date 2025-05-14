bc4bde2e03313ac686e00425dc35b40e9d223a37
Davi-Bernardo761 <davibernardotrabalho@gmail.com>
2025-05-14 20:03:28 -0300

    Update TriangleIdentifier.cpp

--- TriangleIdentifier.cpp
+++ TriangleIdentifier.cpp
@@ -1,4 +1,4 @@
-//This program receives the lenghts of 3 sides of a triangle and tells the user which type of triangle it is.
+//This program receives the lengths of 3 sides of a triangle and tells the user which type of triangle it is.
 #include <iostream>
 #include <string>
 using namespace std;
